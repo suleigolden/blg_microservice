de92aa5851685fe67b7237479b62f9d99fc466f2
suleigoldenboy <suleimamman@gmail.com>
2021-08-11 07:26:29 +0100

    post end points setup

---
+++ post/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/posts', (reg, res) =>{
+
+});
+
+app.post('/posts', (reg, res) =>{
+
+});
+
+app.listen(4000, () => {
+    console.log('listening on port 4000');
+});
