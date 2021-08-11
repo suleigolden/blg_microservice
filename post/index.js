c1a9f3a1f19138dd3d16e791e33fca2ccf9882a3
suleigoldenboy <suleimamman@gmail.com>
2021-08-11 07:47:33 +0100

    post endpoint completed

--- post/index.js
+++ post/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const {randomBytes} = require('crpto');
+
 const app = express();
+app.use(bodyParser.json());
 
+const posts = {};
 
-app.get('/posts', (reg, res) =>{
-
+app.get('/posts', (req, res) =>{
+    res.send(posts);
 });
 
-app.post('/posts', (reg, res) =>{
+app.post('/posts', (req, res) =>{
+    const id = randomBytes(4).toString('hex ');
+    const {title} = req.body.title;
 
+    posts[id] = {
+        id, title
+    };
+
+    res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {
