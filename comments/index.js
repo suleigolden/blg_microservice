733d0914dea8d009ba0a1d731fd81ae38f8a9c7a
suleigoldenboy <suleimamman@gmail.com>
2021-08-12 04:47:45 +0100

    comment service setup

--- comments/index.js
+++ comments/index.js
@@ -8,11 +8,17 @@ app.use(bodyParser.json());
 const commentById = {};
 
 app.get('/posts/:id/comments', (req, res) =>{
-   
+   res.send(commentById[req.params.id] || []);
 });
 
 app.post('/posts/:id/comments', (req, res) =>{
-   
+    const commentId = uuid.v4();
+    const {content} = req.body;
+    const comments = commentById[req.params.id] || [];
+    
+    comments.push({id:commentId, content })
+    commentById[req.params.id] = comments;
+    res.status(201).send(comments);
 });
 
 app.listen(40001, () => {
