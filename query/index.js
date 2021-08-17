54e2fdec749f3a190f6cb153bc86175c890abb09
suleigoldenboy <suleimamman@gmail.com>
2021-08-17 01:17:41 +0100

    Setting up query service alomost completed

--- query/index.js
+++ query/index.js
@@ -2,11 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
 
 const posts = {};
 app.get('posts', (req,res) =>{
