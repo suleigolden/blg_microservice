e4ecf2e8cc93c12436d16e9600eb6adf6cf4ca37
suleigoldenboy <suleimamman@gmail.com>
2021-08-22 00:53:57 +0100

    creation of comment moderation service completed

--- moderation/index.js
+++ moderation/index.js
@@ -6,8 +6,23 @@ const app = express();
 app.use(bodyParser.json());
 
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
+    const {type, data} = req.body;
+    if(type === "CommentCreated"){
+        const status = data.comment.includes('orange') ? 'rejected' : 'approved';
+        await axios.post('http://localhost:4005/events',{
+            type: 'CommentCreated',
+            data:{
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
 
+        });
+    }
+
+    res.send({});
 });
 
 
